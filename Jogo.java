305b9e94da62a4e34fc078bf346e55a5cb32e9c0
Pedro Lopes <aluno@IPIRANGA.FATEC>
2023-09-28 18:14:03 -0300

    p1(jogo):batalha de personagens

--- Jogo.java
+++ Jogo.java
@@ -4,7 +4,7 @@ public class Jogo {
   public static void main(String[] args) throws Exception {
     var gerador = new Random();
     var personagem = new Personagem("John");
-    var personagem2 = new Personagem("Kenjinho",1,1,1);
+    var personagem2 = new Personagem("Kenjinho",3,3,3);
     // var soneca = new Personagem("Soneca", 0, 4, 10);
 
 
@@ -14,11 +14,12 @@ public class Jogo {
     //10% de comer
    
     while(personagem.vivo()&&personagem2.vivo()){
+      
       if(!personagem.vivo() && !personagem2.vivo()){
           System.out.println("morreu");
           break;
       }
-      int oQueFazer = gerador.nextInt(3); //[0..3[
+      int oQueFazer = gerador.nextInt(4); //[0..3[
         
         
       switch(oQueFazer){
@@ -34,7 +35,17 @@ public class Jogo {
         personagem.dormir();
         personagem2.comer();
         break;
+        case 3:
+        int qmAtaca = gerador.nextInt(3);
+      if(qmAtaca == 0){
+        personagem.atacar(personagem2);
+      }else{
+        personagem2.atacar(personagem);
       }
+      break;
+        
+      }
+      
     
       personagem.exibirEstado();
       System.out.println("--------------------");
